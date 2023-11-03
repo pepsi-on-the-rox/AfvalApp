cd6704269b52a3eb436651ed1cf566af3e240925
Noah <Noah@LAPTOPPIE>
2023-11-03 13:33:58 +0100

    Fixed Startup Error with dupped Controller

--- AfvalApi/Program.cs
+++ AfvalApi/Program.cs
@@ -1,4 +1,6 @@
 using DataAccessService;
+using DataContext;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ChillApplicationContext>(options => options.UseSqlServer("Server=.;Database=ChillApplicationContext-04f542cb-7874-4345-a660-3d82a7e52118;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;"));
 builder.Services.AddScoped<DAS>();
 
 var app = builder.Build();
