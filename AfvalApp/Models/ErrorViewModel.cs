9b03903948cf05abe0f2a7e3cd0fd3b9b54e02c5
Agressie <104032229+Agressie@users.noreply.github.com>
2023-11-06 09:50:13 +0100

    Delete AfvalApp directory

--- AfvalApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AfvalApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
