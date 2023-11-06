6b523f1d46c145df6060958e213a634e6f103eb6
Agressie <noah@lenkens.nl>
2023-11-06 16:37:04 +0100

    Added all properties for Issue

--- Models/Issue.cs
+++ Models/Issue.cs
@@ -8,5 +8,16 @@ namespace Models
 {
     public class Issue
     {
+        public int Id { get; set; }
+        public string? PicturePath {  get; set; }
+        public bool State { get; set; } = false;
+        public DateTime Createdate {  get; set; } = DateTime.Now;
+        public DateTime? Resolveddate { get; set; }
+        public int? X1 { get; set; }
+        public int? X2 { get; set; }
+        public int? Y1 { get; set; }
+        public int? Y2 { get; set; }
+        public Label? Label { get; set; }
+        public Operator? Operator { get; set; }
     }
 }
