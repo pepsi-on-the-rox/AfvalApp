49c0f58676690c2659c60917fc699e566e5707ca
Agressie <noah@lenkens.nl>
2023-11-06 16:22:44 +0100

    Added Nuget Packages and Added Code for DB context

---
+++ DataContext/ChillDBcontext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using 
+
+namespace DataContext
+{
+    public class ChillApplicationContext : DbContext
+    {
+        public ChillApplicationContext()
+        {
+
+        }
+
+        public ChillApplicationContext(DbContextOptions<ChillApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ChillApplicationContext-04f542cb-7874-4345-a660-3d82a7e52118;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Operator> Operator { get; set; }
+        public DbSet<Issue> Issue { get; set; }
+        public DbSet<Label> Category { get; set; }
+    }
+}
