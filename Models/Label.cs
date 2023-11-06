2f58afafd32370a77fbcc632eeb292cefd815b00
Agressie <noah@lenkens.nl>
2023-11-06 16:34:08 +0100

    Added all Label properties

--- Models/Label.cs
+++ Models/Label.cs
@@ -8,5 +8,8 @@ namespace Models
 {
     public class Label
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
