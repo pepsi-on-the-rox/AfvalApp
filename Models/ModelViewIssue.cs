373463daafae252c8b16ed317c5a522f12311782
Nils Schagen <1902962schagen@zuyd.nl>
2023-11-08 13:16:01 +0100

    Label selecteren in Edit Issue

--- Models/ModelViewIssue.cs
+++ Models/ModelViewIssue.cs
@@ -9,6 +9,7 @@ namespace Models
     public class ModelViewIssue
     {
         public int? SelectedOperatorId { get; set; }
+        public int? SelectedLabelId { get; set; }
         public List<Operator>? Operators { get; set; }
         public List<Label>? Labels { get; set; }
 
