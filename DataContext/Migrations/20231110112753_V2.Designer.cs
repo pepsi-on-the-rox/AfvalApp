fcd1a073c9e0f4e1d7d49c42e0a282fa9f685b16
Nils Schagen <1902962schagen@zuyd.nl>
2023-11-10 13:41:51 +0100

    continue

--- DataContext/Migrations/20231110112753_V2.Designer.cs
+++ DataContext/Migrations/20231110112753_V2.Designer.cs
@@ -4,6 +4,7 @@ using DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataContext.Migrations
 {
     [DbContext(typeof(ChillApplicationContext))]
-    partial class ChillApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20231110112753_V2")]
+    partial class V2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
